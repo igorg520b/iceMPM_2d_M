972488438bb0875d9fe5b17d4ce8cb0e73673996
Igor Gribanov <igor1215@gmail.com>
2025-09-23 11:16:06 +0900

    moved compressor to separate project

---
+++ compressor/compressor_alt2.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdint>
+#include <sys/stat.h>
+#include <hdf5.h>
+#include <ctime>
+
+// Grid datasets to compress
+const std::vector<std::string> GRID_DATASET_NAMES = {
+    "grid_idx_px", "grid_idx_py", "grid_idx_mass", "grid_idx_vis_pts_density",
+    "grid_idx_vis_Jpinv", "grid_idx_vis_P", "grid_idx_vis_Q",
+    "grid_idx_vis_strain_vonMises"
+};
+
+void process_frame_file(const std::string& inputFile, const std::string& outputFile);
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0]
+                  << " <project_directory> <frame_from> <frame_to>" << std::endl;
+        return 1;
+    }
+
+    std::string projectDir = argv[1];
+    int frame_from = std::stoi(argv[2]);
+    int frame_to   = std::stoi(argv[3]);
+
+    if (frame_from > frame_to) {
+        std::cerr << "Error: frame_from must be <= frame_to" << std::endl;
+        return 1;
+    }
+
+    std::string framesDir = projectDir + "/frames";
+    std::string outDir    = projectDir + "/frames_compressed";
+
+    // Make sure output directory exists
+    std::string mkdir_cmd = "mkdir -p " + outDir;
+    system(mkdir_cmd.c_str());
+
+    std::cout << "Processing frames from " << frame_from << " to "
+              << frame_to << std::endl;
+
+
+    for (int frame = frame_from; frame <= frame_to; ++frame) {
+        std::ostringstream fname;
+        fname << "f" << frame << ".h5";
+
+        std::string inputFile  = framesDir + "/" + fname.str();
+        std::string outputFile = outDir + "/" + fname.str();
+
+        process_frame_file(inputFile, outputFile);
+    }
+
+    return 0;
+}
+
+void process_frame_file(const std::string& inputFile, const std::string& outputFile) {
+    hid_t fsrc = H5Fopen(inputFile.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+    if (fsrc < 0) {
+        std::cerr << "  Could not open " << inputFile << std::endl;
+        return;
+    }
+
+    hid_t fdst = H5Fcreate(outputFile.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+    // --- RGB dataset ---
+    hid_t dset = H5Dopen(fsrc, "rgb", H5P_DEFAULT);
+    if (dset >= 0) {
+        hid_t space = H5Dget_space(dset);
+        hssize_t npoints = H5Sget_simple_extent_npoints(space);
+
+        std::vector<uint8_t> buf(npoints);
+        H5Dread(dset, H5T_NATIVE_UINT8, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf.data());
+
+        hsize_t chunk_dims[3] = {128, 128, 3};
+        hid_t plist = H5Pcreate(H5P_DATASET_CREATE);
+        H5Pset_chunk(plist, 3, chunk_dims);
+        H5Pset_deflate(plist, 8);
+
+        hid_t dset_new = H5Dcreate(fdst, "rgb", H5T_NATIVE_UINT8, space,
+                                   H5P_DEFAULT, plist, H5P_DEFAULT);
+        H5Dwrite(dset_new, H5T_NATIVE_UINT8, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf.data());
+
+        // Copy attributes
+        if (H5Aexists(dset, "SimulationStep") > 0) {
+            int simStep;
+            hid_t attr = H5Aopen(dset, "SimulationStep", H5P_DEFAULT);
+            H5Aread(attr, H5T_NATIVE_INT, &simStep);
+            hid_t attr_new = H5Acreate(dset_new, "SimulationStep", H5T_NATIVE_INT,
+                                       H5Screate(H5S_SCALAR), H5P_DEFAULT, H5P_DEFAULT);
+            H5Awrite(attr_new, H5T_NATIVE_INT, &simStep);
+            H5Aclose(attr);
+            H5Aclose(attr_new);
+        }
+
+        if (H5Aexists(dset, "SimulationTime") > 0) {
+            double simTime;
+            hid_t attr = H5Aopen(dset, "SimulationTime", H5P_DEFAULT);
+            H5Aread(attr, H5T_NATIVE_DOUBLE, &simTime);
+            hid_t attr_new = H5Acreate(dset_new, "SimulationTime", H5T_NATIVE_DOUBLE,
+                                       H5Screate(H5S_SCALAR), H5P_DEFAULT, H5P_DEFAULT);
+            H5Awrite(attr_new, H5T_NATIVE_DOUBLE, &simTime);
+            H5Aclose(attr);
+            H5Aclose(attr_new);
+        }
+
+        H5Pclose(plist);
+        H5Dclose(dset_new);
+        H5Sclose(space);
+        H5Dclose(dset);
+    }
+
+    // --- Grid datasets ---
+    for (auto& name : GRID_DATASET_NAMES) {
+        if (H5Lexists(fsrc, name.c_str(), H5P_DEFAULT) <= 0) continue;
+
+        hid_t dsetg = H5Dopen(fsrc, name.c_str(), H5P_DEFAULT);
+        hid_t space = H5Dget_space(dsetg);
+        hssize_t npoints = H5Sget_simple_extent_npoints(space);
+
+        std::vector<float> buf(npoints);
+        H5Dread(dsetg, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf.data());
+
+        hsize_t chunk_dims[2] = {256, 256};
+        hid_t plist = H5Pcreate(H5P_DATASET_CREATE);
+        H5Pset_chunk(plist, 2, chunk_dims);
+        H5Pset_deflate(plist, 4);
+
+        hid_t dset_new = H5Dcreate(fdst, name.c_str(), H5T_NATIVE_FLOAT, space,
+                                   H5P_DEFAULT, plist, H5P_DEFAULT);
+        H5Dwrite(dset_new, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf.data());
+
+        H5Pclose(plist);
+        H5Dclose(dset_new);
+        H5Sclose(space);
+        H5Dclose(dsetg);
+    }
+
+    H5Fclose(fdst);
+    H5Fclose(fsrc);
+
+    std::cout << "  Successfully compressed: " << inputFile << std::endl;
+}
